c1e39c26dc86d565536897af6616a94fc2e3e0d4
nicoponcet92@gmail.com <nicoponcet92@gmail.com@663101a4-9427-f046-a2b4-05d56739858b>
2014-06-11 01:41:33 +0000

    Inicio caso

---
+++ TP2/src/algo3/modelo/caso/Caso.java
@@ -0,0 +1,17 @@
+package algo3.modelo.caso;
+
+public class Caso {
+	
+	/* 
+	 * Pre: Crear objeto robado a partir de la categoria del jugador 
+	 * 		(novato->objetoComun; detective/investigador->objetoValioso; sargento->objetoMuyValioso).
+	 * 		Crear Ladrón y asignarle dicho objeto.
+	 * 1ro: cantidad de ciudades a generar a partir del valor del objeto robado.
+	 * 2do: generar ciudades.
+	 * 3ro: linkear ciudades (dar referencias a la ciudad sobre la siguiente - casos especiales: primera, ultima).
+	 * 4to: situar ladron en la primer ciudad.
+	 * 5to: 
+	 * 
+	 */
+
+}
