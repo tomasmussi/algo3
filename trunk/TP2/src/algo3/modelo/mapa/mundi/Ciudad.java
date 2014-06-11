502fdf810e74d8a61ef221d9be09983374ededea
tomasmussi@gmail.com <tomasmussi@gmail.com@663101a4-9427-f046-a2b4-05d56739858b>
2014-06-11 19:54:38 +0000

    Mejore la clase Recorrido. Tuve que tocar un par de las otras clases
    Ciudad: sobreescribi el equals y el hashcode para comparar ciudades
    
    Ladron: en Recorrido habia como una especie de iterador sobre donde estaba el ladron, que reemplace con un iterador propio en Ladron, en vez de que la informacion la tenga Recorrido

--- trunk/TP2/src/algo3/modelo/mapa/mundi/Ciudad.java
+++ trunk/TP2/src/algo3/modelo/mapa/mundi/Ciudad.java
@@ -1,5 +1,6 @@
 package algo3.modelo.mapa.mundi;
 
+
 public class Ciudad {
 
 	private int coordenadaX;
@@ -71,7 +72,31 @@ public class Ciudad {
 		this.edificio1 = EdificioFactory.crearEdificioFinanciero(infoCiudad);
 		this.edificio2 = EdificioFactory.crearEdificioCultural(infoCiudad);
 		this.edificio3 = EdificioFactory.crearEdificioDeViaje(infoCiudad);
+	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ciudad other = (Ciudad) obj;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
 	}
 
 }
