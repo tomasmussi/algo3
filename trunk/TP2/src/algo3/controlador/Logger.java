31b966816b8bfa9aec3689ebe2f78e3f4c4c89b3
tomasmussi@gmail.com <tomasmussi@gmail.com@663101a4-9427-f046-a2b4-05d56739858b>
2014-06-11 20:14:31 +0000

    Arme una estructura basica de lo que seria el punto de entrada a la aplicacion

---
+++ trunk/TP2/src/algo3/controlador/Logger.java
@@ -0,0 +1,20 @@
+package algo3.controlador;
+
+import java.io.IOException;
+
+public class Logger {
+
+	public static void loguearWarning(IOException e) {
+		//TODO: Donde loguear??
+	}
+
+	public static void loguearError(IOException e) {
+		//TODO: Donde loguear??
+	}
+
+	public static void loguearError(String string, IOException e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- trunk/TP2/src/algo3/controlador/XMLParser.java
+++ trunk/TP2/src/algo3/controlador/XMLParser.java
@@ -8,11 +8,11 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 
-import algo3.modelo.ladron.Ladron;
+import algo3.modelo.ladron.CaracteristicaLadron;
 
 import com.thoughtworks.xstream.XStream;
 
-public class XMLCodec {
+public class XMLParser {
 
 	public static void main(String[] args) throws IOException {
 		leerLadrones();
@@ -23,11 +23,11 @@ public class XMLCodec {
 	@SuppressWarnings("unchecked")
 	private static void leerLadrones() throws IOException {
 		InputStream in = Properties.class.getResourceAsStream("/algo3/controlador/ladrones.xml");
-		List<Ladron> ladrones = (List<Ladron>)xmlReader.fromXML(in);
+		List<CaracteristicaLadron> ladrones = (List<CaracteristicaLadron>)xmlReader.fromXML(in);
 		in.close();
-		Iterator<Ladron> it = ladrones.iterator();
+		Iterator<CaracteristicaLadron> it = ladrones.iterator();
 		while (it.hasNext()){
-			Ladron ladron = it.next();
+			CaracteristicaLadron ladron = it.next();
 			System.out.println(ladron);
 		}
 	}
@@ -50,8 +50,32 @@ public class XMLCodec {
 	}
 
 	public static Object decode() throws IOException {
-		InputStream in = Properties.class.getResourceAsStream("/algo3/controlador/ladrones.xml");
-		return xmlReader.fromXML(in);
+		Object decodificado = null;
+		try{
+			InputStream in = Properties.class.getResourceAsStream("/algo3/controlador/ladrones.xml");
+			decodificado = xmlReader.fromXML(in);
+			if (in != null){
+				in.close();
+			}
+		} catch (IOException e){
+			Logger.loguearError("Error al cargar configuracion por defecto", e);
+		}
+		return decodificado;
+	}
+
+
+	public static Object decode(InputStream in) throws IOException {
+		Object decodificado = null;
+		try{
+			decodificado = xmlReader.fromXML(in);
+			if (in != null){
+				in.close();
+			}
+		} catch (IOException e){
+			Logger.loguearWarning(e);
+			decodificado = XMLParser.decode();
+		}
+		return decodificado;
 	}
 
 }
