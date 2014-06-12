4fe6cb031470ca0828172d6b1d06685939a1b673
flaviaacoria <flaviaacoria@gmail.com>
2014-06-12 10:37:28 +0000

    implementé lógica de generarOrdenDeArresto

--- TP2/src/algo3/modelo/caso/Caso.java
+++ TP2/src/algo3/modelo/caso/Caso.java
@@ -1,7 +1,15 @@
 package algo3.modelo.caso;
 
+import java.util.List;
+
+import algo3.modelo.estacionPolicia.EstacionDePolicia;
+import algo3.modelo.ladron.CaracteristicaLadron;
+import algo3.modelo.ladron.Ladron;
+import algo3.modelo.objeto.Objeto;
+import algo3.modelo.policia.OrdenDeArresto;
+
 public class Caso {
-	
+
 	/* 
 	 * Pre: Crear objeto robado a partir de la categoria del jugador 
 	 * 		(novato->objetoComun; detective/investigador->objetoValioso; sargento->objetoMuyValioso).
@@ -13,5 +21,58 @@ public class Caso {
 	 * 5to: 
 	 * 
 	 */
+	
+	private OrdenDeArresto ordenDeArresto;
+	private Ladron ladron;
+	private Objeto objeto;
+
+	
+
+	public boolean generarOrdenDeArresto(CaracteristicaLadron caracteristica  ){
+		
+		List<CaracteristicaLadron> expedientes = EstacionDePolicia.getInstance().buscarExpediente(caracteristica);
+		if (expedientes.size()==1){
+			try {
+				this.ordenDeArresto = new OrdenDeArresto((CaracteristicaLadron)expedientes.get(0).clone());
+				return true;
+			}catch (CloneNotSupportedException e){
+				System.out.println("Hubo un error inesperado en el programa\n");
+				
+			}
+
+		}
+		return false;
+	}
+	
+	public void asignarCiudades(){
+		//TODO: A IMPLEMENTAR
+	}
+	
+	
+	public OrdenDeArresto getOrdenDeArresto() {
+		return ordenDeArresto;
+	}
+	public void setOrdenDeArresto(OrdenDeArresto ordenDeArresto) {
+		this.ordenDeArresto = ordenDeArresto;
+	}
+	public Ladron getLadron() {
+		return ladron;
+	}
+	public void setLadron(Ladron ladron) {
+		this.ladron = ladron;
+	}
+	public Objeto getObjeto() {
+		return objeto;
+	}
+	public void setObjeto(Objeto objeto) {
+		this.objeto = objeto;
+	}
+	
+	
+	
 
 }
+
+	
+
+
