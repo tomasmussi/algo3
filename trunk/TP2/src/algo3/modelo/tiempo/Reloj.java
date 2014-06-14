016178c49ecfdc2db515052e01a29c92d0210305
tomasmussi@gmail.com <tomasmussi@gmail.com@663101a4-9427-f046-a2b4-05d56739858b>
2014-06-14 20:43:44 +0000

    Reloj andando, ahora viene el refactor de Policia

--- trunk/TP2/src/algo3/modelo/tiempo/Reloj.java
+++ trunk/TP2/src/algo3/modelo/tiempo/Reloj.java
@@ -5,6 +5,7 @@ import java.text.DecimalFormat;
 public class Reloj {
 
 	private static final DecimalFormat formato = new DecimalFormat("00");
+	private static final int HORAS_A_DORMIR = 8;
 	private String[] dias = {"Lunes", "Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"};
 	private int numeroDia;
 	private int horaDia;
@@ -18,7 +19,8 @@ public class Reloj {
 		return dias[numeroDia] + " " + formato.format(horaDia) + ":00 horas";
 	}
 
-	public void transcurrir(int horas) {
+	public void transcurrirRecursivo(int horas, boolean todaviaNoDurmio){
+
 		int horaTotal = horaDia + horas;
 		int cantidadDiasPasados = horaTotal / 24;
 		int horaActual = horaTotal % 24;
@@ -35,7 +37,22 @@ public class Reloj {
 			numeroDia = 6;
 			horaDia = 17;
 		}
+		if (esDeNoche() && !todaviaNoDurmio){
+			transcurrirRecursivo(HORAS_A_DORMIR, true);
+		}
+
+
 	}
+
+	public void transcurrir(int horas) {
+		transcurrirRecursivo(horas, false);
+	}
+
+	private boolean esDeNoche(){
+		return horaDia >= 23 || horaDia <= 7;
+	}
+
+
 
 	public boolean hayTiempoRestante(){
 		return ! (numeroDia == 6 && horaDia == 17);
