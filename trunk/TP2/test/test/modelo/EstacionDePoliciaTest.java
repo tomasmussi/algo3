3bf555e64f31ad4939afdcfb52f0681714eca318
flaviaacoria@gmail.com <flaviaacoria@gmail.com@663101a4-9427-f046-a2b4-05d56739858b>
2014-06-15 19:14:15 +0000

    prueba de q si no selecciona ninguna caracteristica para la orden de arresto devuelve todos los ladrones

--- trunk/TP2/test/test/modelo/EstacionDePoliciaTest.java
+++ trunk/TP2/test/test/modelo/EstacionDePoliciaTest.java
@@ -28,11 +28,19 @@ public class EstacionDePoliciaTest {
 	}
 
 	@Test
-	public void testvariasCoincidenciaConExpedientesDeEstacionPolicia(){
+	public void testVariasCoincidenciaConExpedientesDeEstacionPolicia(){
 
 		List<CaracteristicaLadron> expedientes= EstacionDePolicia.getInstance().buscarExpediente(new CaracteristicaLadron( null, "Masculino", "Mountain Climbing", null, null, "Motocicleta"));
 
 		assertEquals(expedientes.size(), 2);
+	}	
+		
+	@Test
+	public void testCargarSinCaracteristicasDevuelveTodosLosLadrones(){
+		
+		List<CaracteristicaLadron> expedientes= EstacionDePolicia.getInstance().buscarExpediente(new CaracteristicaLadron( null, null, null, null, null, null));
+		assertEquals(expedientes.size(), 4);
+
 	}
 
 }
