9496bb1c314d2770b7610981177ad02c465cbafd
tomasmussi <tomasmussi@gmail.com>
2014-06-18 02:38:03 +0000

    Agregue comentarios en EstacionDePolicia y agregue un test en CapturaTest para probar que genero orden de arresto a partir de una caracteristica y me devuelve el expediente entero con TODAS las caracteristicas del ladron.

--- TP2/test/test/modelo/integracion/CapturaTest.java
+++ TP2/test/test/modelo/integracion/CapturaTest.java
@@ -1,16 +1,17 @@
 package test.modelo.integracion;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import algo3.modelo.estacionPolicia.EstacionDePolicia;
 import algo3.modelo.ladron.CaracteristicaLadron;
 import algo3.modelo.ladron.Ladron;
-import algo3.modelo.objeto.CaracteristicaObjeto;
-import algo3.modelo.objeto.ObjetoComun;
-import algo3.modelo.objeto.Robable;
 import algo3.modelo.policia.Policia;
 import algo3.modelo.tiempo.Reloj;
 
@@ -18,20 +19,20 @@ import algo3.modelo.tiempo.Reloj;
 public class CapturaTest {
 
 	/* ************** Métodos auxiliares ************** */
-	
-	private CaracteristicaLadron caracteristica1;
-	private CaracteristicaLadron caracteristica2;
-	private Ladron ladron;
-	private Robable objeto;
+
+	private CaracteristicaLadron caracteristicaNickBrunch;
+	private CaracteristicaLadron caracteristicaMereyLaroc;
+	private CaracteristicaLadron caracteristicaUnivocaNickBrunch;
+	private Ladron nickBrunch;
 	private Reloj reloj;
 	private Policia policia;
-	
+
 	@Before
 	public void  initialize() {
-		caracteristica1 = new CaracteristicaLadron("Nick Brunch", "Masculino", "Mountain Climbing", "Negro", "Anillo", "Motocicleta");
-		caracteristica2 = new CaracteristicaLadron("Merey Laroc", "Femenino", "Croquet", "Marron", "Joyas", "Limusina");
-		ladron = new Ladron(caracteristica1);
-		objeto = new ObjetoComun(new CaracteristicaObjeto("Buda dorado", "Bangkok"));
+		caracteristicaNickBrunch = new CaracteristicaLadron("Nick Brunch", "Masculino", "Mountain Climbing", "Negro", "Anillo", "Motocicleta");
+		caracteristicaMereyLaroc = new CaracteristicaLadron("Merey Laroc", "Femenino", "Croquet", "Marron", "Joyas", "Limusina");
+		caracteristicaUnivocaNickBrunch = new CaracteristicaLadron(null, null, null, "Negro", null, "Motocicleta");
+		nickBrunch = new Ladron(caracteristicaNickBrunch);
 		reloj = new Reloj();
 		policia = new Policia(reloj);
 	}
@@ -40,37 +41,41 @@ public class CapturaTest {
 
 	@Test
 	public void testPoliciaNoAtrapaLadronSinTiempoRestante(){
-
 		//Se quedo sin tiempo para emitir la orden de arresto.
 		reloj.transcurrir(154);
 		// Para crear la  orden de arresto lo hace con las caracteristicas del ladron que esta en base de datos.
-		assertFalse(policia.emitirOrdenDeArresto(caracteristica1));
-
+		assertFalse(policia.emitirOrdenDeArresto(caracteristicaNickBrunch));
 	}
 
 	@Test
 	public void testPoliciaNoAtrapaLadronSinOrdenDeArresto() {
-		assertFalse( policia.arrestar(ladron));
-
+		assertFalse(policia.arrestar(nickBrunch));
 	}
 
 	@Test
 	public void testPoliciaNoAtrapaLadronConOrdenDeArrestoEquivocada(){
-		
 		//Crea Orden de arresto con caracteristicas que no son las del ladron juego pero que esta en la base de datos
-		assertTrue(policia.emitirOrdenDeArresto(caracteristica2));
+		assertTrue(policia.emitirOrdenDeArresto(caracteristicaMereyLaroc));
 		//No atrapa ladron porque no coincide con la orden de arresto.
-		assertFalse(policia.arrestar(ladron));
+		assertFalse(policia.arrestar(nickBrunch));
 	}
 
 	@Test
 	public void testPoliciaAtrapaLadronConTiempoYOrdenDeArrestoCorrecta(){
-		
 		reloj.transcurrir(148);
 		// Para crear la  orden de arresto lo hace con las caracteristicas del ladron (En este caso el que cree al inicio).
-		assertTrue(policia.emitirOrdenDeArresto(caracteristica1));
+		assertTrue(policia.emitirOrdenDeArresto(caracteristicaNickBrunch));
 		//La orden de arresto coincide con el ladron del juego y lo arresta
-		assertTrue(policia.arrestar(ladron));
+		assertTrue(policia.arrestar(nickBrunch));
 	}
+
+	@Test
+	public void testPoliciaIdentificaLadronConOrdenDeArrestoQueCoincideConLadron(){
+		List<CaracteristicaLadron> expedientes = EstacionDePolicia.getInstance().buscarExpediente(caracteristicaUnivocaNickBrunch);
+		assertEquals(1, expedientes.size());
+		CaracteristicaLadron caracteristicaObtenida = expedientes.get(0);
+		assertTrue(caracteristicaNickBrunch.equals(caracteristicaObtenida));
+	}
+
 
 }
