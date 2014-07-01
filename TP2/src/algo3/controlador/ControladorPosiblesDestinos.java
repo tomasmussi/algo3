d1b19d07627578ac917f3526b856f42b216d9338
eliperisset <eliperisset@gmail.com>
2014-07-01 18:15:18 +0000

    A ver si les gusta lo que hice.... Hay que ajustar la foto de la ciudad pero no sabia bien como hacerlo.. La idea es que el mapa quede siempre atras asi no hay que cargarlo todo el tiempo, y la foto de la ciudad este arriba y desaparezca cdo se aprieta una de las opc: viajar o ver destinos posibles.

--- TP2/src/algo3/controlador/ControladorPosiblesDestinos.java
+++ TP2/src/algo3/controlador/ControladorPosiblesDestinos.java
@@ -6,17 +6,22 @@ import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
 
 import algo3.modelo.juego.Juego;
+import algo3.vista.FrameJuego;
 
 public class ControladorPosiblesDestinos implements ActionListener {
 
+	private FrameJuego frame;
 	private Juego juego;
 
-	public ControladorPosiblesDestinos(Juego juego){
+	public ControladorPosiblesDestinos(FrameJuego frameJuego, Juego juego) {
+		this.frame = frameJuego;
 		this.juego = juego;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		frame.mostrarGoogleMaps();
+		frame.refrescarMarcadores();
 		String[] ciudadesPosibles = juego.getCiudadesPosibles();
 		JOptionPane.showMessageDialog(null, ciudadesPosibles, "Ciudades posibles", JOptionPane.INFORMATION_MESSAGE);
 	}
